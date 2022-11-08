cdd884bbaae81e9c137d5a3480078ef7946b81aa
LittleRookey <37283117+LittleRookey@users.noreply.github.com>
2022-11-07 22:22:05 -0500

    runbackward animation
    - made run backward animation using blendtree
    - integrated forward and backward animation

--- Realtime Coop Roguelike Defense/Assets/Scripts/PlayerMovement.cs
+++ Realtime Coop Roguelike Defense/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerMovement : AIMovement
 {
     [SerializeField] private Rigidbody2D rb;
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
     protected override void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -21,10 +27,16 @@ public class PlayerMovement : AIMovement
         if (!canMove) return;
         float _x = Input.GetAxisRaw("Horizontal");
         float _y = Input.GetAxisRaw("Vertical");
-        
-        if (_x == 0 && _y == 0) return;
 
+        if (_x == 0 && _y == 0) 
+        {
+            anim.SetBool("isRun", false);
+            return;
+            
+        }
+        anim.SetBool("isRun", true);
         directionVec = new Vector2(_x, _y).normalized;
+        anim.SetFloat("VelocityX", directionVec.x);
         transform.localPosition += directionVec * _moveSpeed * Time.deltaTime;
 
     }
